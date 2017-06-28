839755d31ddf6d1f94c383ecd13f0c93efbb459e
Stephen Newman <steve@newman.digital>
2017-06-28 14:54:32 +0100

    Prevent the elementType from being cracked open for generic arguments if the creator doesn't require multiple arguments itself.

--- Hyperion.Tests/ImmutableCollectionsTests.cs
+++ Hyperion.Tests/ImmutableCollectionsTests.cs
@@ -11,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
+using FluentAssertions;
 using Xunit;
 
 namespace Hyperion.Tests
@@ -19,17 +20,19 @@ namespace Hyperion.Tests
     {
         #region internal classes
 
-        class TestClass
+        class TestClass<T, U>
         {
             // KeyValuePair<int, int> because Mono has broken structural equality on KeyValuePair<,>
-            public IImmutableDictionary<int, int> Dictionary { get; }
-            public IImmutableList<string> List { get; }
-            public IImmutableQueue<string> Queue { get; }
-            public IImmutableSet<string> SortedSet { get; }
-            public IImmutableSet<string> HashSet { get; }
-            public IImmutableStack<string> Stack { get; }
+            public IImmutableDictionary<int, T> Dictionary { get; }
 
-            public TestClass(IImmutableDictionary<int, int> dictionary, IImmutableList<string> list, IImmutableQueue<string> queue, IImmutableSet<string> sortedSet, IImmutableSet<string> hashSet, IImmutableStack<string> stack)
+            public IImmutableList<U> List { get; }
+            public IImmutableQueue<U> Queue { get; }
+            public IImmutableSet<U> SortedSet { get; }
+            public IImmutableSet<U> HashSet { get; }
+            public IImmutableStack<U> Stack { get; }
+
+            public TestClass(IImmutableDictionary<int, T> dictionary, IImmutableList<U> list, IImmutableQueue<U> queue,
+                IImmutableSet<U> sortedSet, IImmutableSet<U> hashSet, IImmutableStack<U> stack)
             {
                 Dictionary = dictionary;
                 List = list;
@@ -39,14 +42,14 @@ namespace Hyperion.Tests
                 Stack = stack;
             }
 
-            protected bool Equals(TestClass other)
+            protected bool Equals(TestClass<T, U> other)
             {
-                return SeqEquals(Dictionary, other.Dictionary) 
-                    && SeqEquals(List, other.List) 
-                    && SeqEquals(Queue, other.Queue) 
-                    && SeqEquals(SortedSet, other.SortedSet) 
-                    && SeqEquals(HashSet, other.HashSet) 
-                    && SeqEquals(Stack, other.Stack);
+                return SeqEquals(Dictionary, other.Dictionary)
+                       && SeqEquals(List, other.List)
+                       && SeqEquals(Queue, other.Queue)
+                       && SeqEquals(SortedSet, other.SortedSet)
+                       && SeqEquals(HashSet, other.HashSet)
+                       && SeqEquals(Stack, other.Stack);
             }
 
             public override bool Equals(object obj)
@@ -54,10 +57,10 @@ namespace Hyperion.Tests
                 if (ReferenceEquals(null, obj)) return false;
                 if (ReferenceEquals(this, obj)) return true;
                 if (obj.GetType() != this.GetType()) return false;
-                return Equals((TestClass) obj);
+                return Equals((TestClass<T, U>) obj);
             }
 
-            private static bool SeqEquals<T>(IEnumerable<T> first, IEnumerable<T> second)
+            private static bool SeqEquals<V>(IEnumerable<V> first, IEnumerable<V> second)
             {
                 var f = first.GetEnumerator();
                 var s = second.GetEnumerator();
@@ -79,6 +82,74 @@ namespace Hyperion.Tests
                 }
 
                 return true;
+            }
+        }
+
+        interface IContainer<T> : IComparer<IContainer<T>>, IComparable
+        {
+            T Value { get; }
+        }
+
+        class Container<T> : IContainer<T>
+        {
+            private readonly T value;
+
+            public T Value => value;
+
+            public Container(T value)
+            {
+                this.value = value;
+            }
+
+            public int Compare(IContainer<T> x, IContainer<T> y)
+            {
+                var comparer = new ContainerComparer<T>(Comparer<T>.Default);
+                return comparer.Compare(x, y);
+            }
+
+            public int CompareTo(object obj)
+            {
+                if (obj == null)
+                {
+                    return 1;
+                }
+
+                if (obj is IContainer<T>)
+                {
+                    return Compare(this, (IContainer<T>) obj);
+                }
+
+                return 1;
+            }
+        }
+
+        class ContainerComparer<T> : IComparer<IContainer<T>>
+        {
+            private readonly IComparer<T> innerComparer;
+
+            public int Compare(IContainer<T> x, IContainer<T> y)
+            {
+                if (x == null && y == null)
+                {
+                    return 0;
+                }
+
+                if (x == null)
+                {
+                    return -1;
+                }
+
+                if (y == null)
+                {
+                    return 1;
+                }
+
+                return innerComparer.Compare(x.Value, y.Value);
+            }
+
+            public ContainerComparer(IComparer<T> innerComparer)
+            {
+                this.innerComparer = innerComparer;
             }
         }
 
@@ -257,22 +328,44 @@ namespace Hyperion.Tests
         [Fact]
         public void CanSerializeImmutableCollectionsReferencedThroughInterfaceInFields()
         {
-            var expected = new TestClass(
+            var expected = new TestClass<int, string>(
                 dictionary: ImmutableDictionary.CreateRange(new[]
                 {
                     new KeyValuePair<int, int>(2, 1),
                     new KeyValuePair<int, int>(3, 2),
                 }),
-                list: ImmutableList.CreateRange(new[] { "c", "d" }),
-                queue: ImmutableQueue.CreateRange(new[] { "e", "f" }),
-                sortedSet: ImmutableSortedSet.CreateRange(new[] { "g", "h" }),
-                hashSet: ImmutableHashSet.CreateRange(new[] { "i", "j" }),
-                stack: ImmutableStack.CreateRange(new[] { "k", "l" }));
+                list: ImmutableList.CreateRange(new[] {"c", "d"}),
+                queue: ImmutableQueue.CreateRange(new[] {"e", "f"}),
+                sortedSet: ImmutableSortedSet.CreateRange(new[] {"g", "h"}),
+                hashSet: ImmutableHashSet.CreateRange(new[] {"i", "j"}),
+                stack: ImmutableStack.CreateRange(new[] {"k", "l"}));
 
             Serialize(expected);
             Reset();
-            var actual = Deserialize<TestClass>();
+            var actual = Deserialize<TestClass<int, string>>();
             Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CanSerializeImmutableCollectionsOfTypesWithGenericParameters()
+        {
+            var comparer = new ContainerComparer<int?>(Comparer<int?>.Default);
+            var expected = new TestClass<IContainer<int?>, IContainer<int?>>(
+                dictionary: ImmutableDictionary.CreateRange(new[]
+                {
+                    new KeyValuePair<int, IContainer<int?>>(2, new Container<int?>(1)),
+                    new KeyValuePair<int, IContainer<int?>>(3, new Container<int?>(2)),
+                }),
+                list: ImmutableList.CreateRange<IContainer<int?>>(new[] {new Container<int?>(3), new Container<int?>(4)}),
+                queue: ImmutableQueue.CreateRange<IContainer<int?>>(new[] {new Container<int?>(5), new Container<int?>(6)}),
+                sortedSet: ImmutableSortedSet.CreateRange<IContainer<int?>>(comparer, new[] {new Container<int?>(7), new Container<int?>(8)}),
+                hashSet: ImmutableHashSet.CreateRange<IContainer<int?>>(new[] {new Container<int?>(9), new Container<int?>(10)}),
+                stack: ImmutableStack.CreateRange<IContainer<int?>>(new[] {new Container<int?>(11), new Container<int?>(12)}));
+
+            Serialize(expected);
+            Reset();
+            var actual = Deserialize<TestClass<IContainer<int?>, IContainer<int?>>>();
+            actual.ShouldBeEquivalentTo(expected);
         }
     }
 }
